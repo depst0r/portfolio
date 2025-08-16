748301b0dbc881d7522e1d07eba1519e80bb8ec8
depstor <depstor90@gmail.com>
2025-08-16 20:07:02 +0300

    swiper

--- src/script/script.js
+++ src/script/script.js
@@ -1,3 +1,32 @@
 import '/src/style/style.scss';
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
 
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
+try {
+    new Swiper('.mySwiper', {
+        modules: [Navigation, Pagination],
+        slidesPerView: 1,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        },
+        loop: true,
+        spaceBetween: 10,
+        breakpoints: {
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 5
+            },
+        },
+    })
+} catch (error) {
+    console.log(error)
+}
