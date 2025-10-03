a4c8b7b69c05e98ed364502cf0a16c7d4ba50a47
depstor <depstor90@gmail.com>
2025-10-03 14:46:29 +0300

    menu-v3

--- src/script/script.js
+++ src/script/script.js
@@ -1,11 +1,21 @@
 import '/src/style/style.scss';
 
 window.addEventListener('DOMContentLoaded', () => {
-    const gamburger = document.querySelector('.header__menu-gamburger')
+    const gamburger = document.querySelector('.header__menu-gamburger'),
+        headerNavSm = document.querySelector('.header__nav-sm')
 
-    gamburger.addEventListener('click', e => {
-        gamburger.classList.toggle('header__menu-gamburger--active')
-    })
+    const elemToggle = (el, selector) => {
+        el.classList.toggle(selector)
+    }
+
+    const hideShowMenu = elem => {
+        elem.addEventListener('click', () => {
+            elemToggle(gamburger, 'header__menu-gamburger--active')
+            elemToggle(headerNavSm, 'header__nav-sm--visible')
+        })
+    }
+
+    hideShowMenu(gamburger)
 })
 
 
