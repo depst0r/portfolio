3579d4e534d07e1c9daea6bce0d7138629c24837
depstor <depstor90@gmail.com>
2025-10-03 11:47:16 +0300

    burger-an

--- src/script/script.js
+++ src/script/script.js
@@ -1,5 +1,13 @@
 import '/src/style/style.scss';
 
+window.addEventListener('DOMContentLoaded', () => {
+    const gamburger = document.querySelector('.header__menu-gamburger')
+
+    gamburger.addEventListener('click', e => {
+        gamburger.classList.toggle('header__menu-gamburger--active')
+    })
+})
+
 
 
 
