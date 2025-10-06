309382fe9756b1f319b79b1c9ad40401bf2abac6
depstor <depstor90@gmail.com>
2025-10-06 10:28:42 +0300

    form-overflove

--- src/script/script.js
+++ src/script/script.js
@@ -2,8 +2,10 @@ import '/src/style/style.scss';
 
 window.addEventListener('DOMContentLoaded', () => {
     const gamburger = document.querySelector('.header__menu-gamburger'),
-        headerNavSm = document.querySelector('.header__nav-sm')
-
+        headerNavSm = document.querySelector('.header__nav-sm'),
+        overlay = document.querySelector('.form__modal-overlay'),
+        closeBtnForm = document.querySelector('.form__close-btn'),
+        btnContact = document.querySelector('.btn-contact');
 
     const elemToggle = (el, selector) => {
         el.classList.toggle(selector)
@@ -16,10 +18,19 @@ window.addEventListener('DOMContentLoaded', () => {
         })
     }
 
+    btnContact.addEventListener('click', () => {
+        overlay.classList.add('form__modal-overlay--active')
+        document.body.classList.add('modal-open');
+
+    })
+
+    closeBtnForm.addEventListener('click', () => {
+        overlay.classList.remove('form__modal-overlay--active')
+        document.body.classList.remove('modal-open');
+    })
 
 
     hideShowMenu(gamburger)
-
 
 })
 
