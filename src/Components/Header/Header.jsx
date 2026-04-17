bb6671be5d379da400792c2f46b4c68c263b04be
depstor <depstor90@gmail.com>
2026-04-17 21:03:18 +0300

    gamburger

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,20 +7,36 @@ import './Header.scss';
 const Header = () => {
 
     const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    // useEffect(() => {
-    //     const headleResize = () => {
-    //         if (isMobile) 
-    //     }
-    // }, [])
+    useEffect(() => {
+        const headleResize = () => {
+            if (window.innerWidth > 768) {
+                setIsMenuOpen(false)
+            }
+        }
+
+        window.addEventListener('resize', headleResize)
+        return () => window.removeEventListener('resize', headleResize)
+    }, [])
 
     return (
     <header className='header'>
         <div className="header__body">
             <div className="header__columns">
             <div className="header__logo">
+            <div className="header__logo-col">
                 <img src={Logo} alt="Logo" />
                 <h6 className="header__logo-name">depstor</h6>
+            </div>
+            <div className="header__logo-col"></div>
+                {isMobile ?  
+                <button type="button">
+                    <div className="gamburger">
+                        <span className="line"></span>
+                        <span className="line"></span>
+                    </div>
+                </button> : null}
             </div>
             { !isMobile ? <HeaderDesktop/> : <HeaderMobile/> }
             </div>
--- src/Components/Header/HeaderMobile.jsx
+++ src/Components/Header/HeaderMobile.jsx
@@ -1,21 +1,30 @@
 const HeaderMobile = () => {
     return (
+        <div className="header__wrapper">
         <nav className="header__navigation-mobile">
-            <ul className="header__menu--mobile">
-                <li className="header__links">
-                    <a href="#" className="header__link">#Home</a>
+            <ul className="header__navigation-menu">
+                <li className="header__navigation-links">
+                    <a href="#" className="header__navigation-link"><span className="simbol">#</span>Home</a>
                     </li>
                 <li className="header__links">
-                    <a href="#" className="header__link">#Works</a>
+                    <a href="#" className="header__navigation-link"><span className="simbol">#</span>Works</a>
                     </li>
                 <li className="header__links">
-                    <a href="#" className="header__link">#About-me</a>
+                    <a href="#" className="header__navigation-link"><span className="simbol">#</span>About-me</a>
                     </li>
                 <li className="header__links">
-                    <a href="#" className="header__link">#Contacts</a>
+                    <a href="#" className="header__navigation-link"><span className="simbol">#</span>Contacts</a>
             </li>
         </ul>
         </nav>
+        <button type="button">
+            <div className="gamburger">
+                <span className="line"></span>
+                <span className="line"></span>
+            </div>
+        </button>
+        </div>
+
     )
 }
 
