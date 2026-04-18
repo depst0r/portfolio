8b658392525034ee801e928e5b5ad709c4ebb8eb
depstor <depstor90@gmail.com>
2026-04-19 01:05:56 +0300

    animation-gamburger

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import {motion, AnimatePresence} from 'framer-motion';
 import HeaderDesktop from '../Header/HeaderDesktop.jsx';
 import HeaderMobile from '../Header/HeaderMobile.jsx';
 import Logo from '../../Images/Logo.png';
@@ -8,6 +9,8 @@ const Header = () => {
 
     const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const togglleMenu = () => setIsMenuOpen(!isMenuOpen) 
 
     useEffect(() => {
         const headleResize = () => {
@@ -29,12 +32,16 @@ const Header = () => {
                 <img src={Logo} alt="Logo" />
                 <h6 className="header__logo-name">depstor</h6>
             </div>
-            <div className="header__logo-col"></div>
+            {/* <div className="header__logo-col"></div> */}
                 {isMobile ?  
-                <button type="button">
-                    <div className="gamburger">
-                        <span className="line"></span>
-                        <span className="line"></span>
+                <button type="button" className='gamburger' onClick={togglleMenu}>
+                    <div className="gamburger__lines">
+                        <motion.span 
+                            animate={{rotate: isMenuOpen ? -45 : 0, y: isMenuOpen ? 9 : 0}}
+                        className="gamburger__line" />
+                        <motion.span 
+                            animate={{rotate: isMenuOpen ? 45 : 0, opacity: isMenuOpen ? 0 : 1}}
+                        className="gamburger__line" />
                     </div>
                 </button> : null}
             </div>
